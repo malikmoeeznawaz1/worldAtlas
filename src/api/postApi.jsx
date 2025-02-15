66fbeedc711fd6d06e43d7e0a7318aeb0919eb57
Malik Moeez Nawaz <malikmoeez.nawaz@studentambassadors.com>
2025-02-15 17:38:27 +0500

    Loader code is in progress and axios code is correctd

--- src/api/postApi.jsx
+++ src/api/postApi.jsx
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://restcountries.com/v3.1"
+  baseURL: "https://restcountries.com/v3.1",
 });
 
 // HTTP GET METHOD 
 export const getCountryData = () => {
-  api.get("/all?fields=name,population,region,capital,flags");
-}
+  return api.get("/all?fields=name,population,region,capital,flags");
+};
--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -1,4 +1,21 @@
+import { useEffect, useState, useTransition } from "react"
+import { getCountryData } from "../api/postApi";
+
 const Country = () => {
+
+  const[isPending, startTransition] = useTransition();
+  const[countries, setCountries] = useState([]);
+
+  useEffect(()=>{
+    startTransition(async()=>{
+      const res = await getCountryData();  
+      console.log(res);
+      setCountries(res.data);      
+    });
+  },[])
+
+  if(isPending) return <Loader/>;
+
   return (
     <h1>Country</h1>
   )
